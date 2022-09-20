313dedbca97e15bbc265f704d4bb9bb3d4841473
KaraevaLinura <linura.karaeva.87@mail.ru>
2022-09-20 11:44:07 +0600

    Решение

--- Program.cs
+++ Program.cs
@@ -7,3 +7,9 @@ Console.WriteLine("Экзамен");
     лучше обойтись исключительно массивами.*/
 string[] array = { "Rus", "QA", "number", "163", "Hello world!", "qwe" };
 string[] result = new string[array.Length];
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3) result[i] = array[i];
+    else result[i] = string.Empty;
+    if (result[i] != string.Empty) Console.Write($"{result[i]} ");
+}
